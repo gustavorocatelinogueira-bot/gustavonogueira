2e90b8d8c2d9147801117f878336d31410d29623
Gustavo <gustavo.rocateli.nogueira@escola.pr.gov.br>
2026-04-09 12:08:43 -0300

    adicionando informaçoes

--- main.js
+++ main.js
@@ -5,3 +5,32 @@ const numero= 5;
 alert("o nome é: "+ nome1);
 alert(" o nome é: "+ nome2);
 console.log(numero);
+
+let num1 = prompt ("Digite o primeiro numero");
+let num2 = prompt ("Digite o segundo numero");
+let num3 = prompt ("Digite o terceiro numero");
+
+function soma (num1,num2,num3){
+    let soma = num1+num2+num2;
+return soma}
+soma();
+
+function sub(num1,num2,num3){
+let sub = num1-num2-num3;
+return sub}
+soma();
+
+function mult (num1,num2,num3){
+let mult = num1*num2*num3;
+return mult}
+mult();
+
+function div (num1,num2,num3){
+    let div = num1/num2/num3;
+    return div}
+    div();
+
+    alert("A soma é" + (num1,num2,num3));
+    alert("A sub é" + (num1,num2,num3));
+    alert("A mult é" + (num1,num2,num3));
+    alert("A div é" + (num1,num2,num3));
